b4a2bcaf2b539b689020e31159ee3238e1b27533
hayleefay <hayleeham@gmail.com>
2015-09-10 19:36:07 -0600

    jQuery buttons

--- build.js
+++ build.js
@@ -1,19 +1,32 @@
+// $(document).ready(function(){
+// 	var changeFontSize = function(fontSize){
+// 		console.log("Goodbye!");
+// 		document.getElementById('paragraph').style.fontSize = fontSize;
+// 	}
+// 	var setFontEventListener = function(fontButton, fontSize){
+// 		console.log("Hello!");
+// 		document.getElementById(fontButton).addEventListener("click",function(){
+// 			changeFontSize(fontSize)
+// 		},false);
+// 	}
+// 	setFontEventListener("fontSize10", "0.625em");
+// 	setFontEventListener("fontSize12", "0.75em");
+// 	setFontEventListener("fontSize14", "0.875em");
+// 	setFontEventListener("fontSize16", "1em");
+// 	setFontEventListener("fontSize20", "1.25em");
+// 	setFontEventListener("fontSize24", "1.5em");
+// 	setFontEventListener("fontSize30", "1.875em");
+// });
+
 $(document).ready(function(){
-	var changeFontSize = function(fontSize){
-		console.log("Goodbye!");
-		document.getElementById('paragraph').style.fontSize = fontSize;
-	}
-	var setFontEventListener = function(fontButton, fontSize){
-		console.log("Hello!");
-		document.getElementById(fontButton).addEventListener("click",function(){
-			changeFontSize(fontSize)
-		},false);
-	}
-	setFontEventListener("fontSize10", "0.625em");
-	setFontEventListener("fontSize12", "0.75em");
-	setFontEventListener("fontSize14", "0.875em");
-	setFontEventListener("fontSize16", "1em");
-	setFontEventListener("fontSize20", "1.25em");
-	setFontEventListener("fontSize24", "1.5em");
-	setFontEventListener("fontSize30", "1.875em");
-});
+	$('.changeSize').on("click", function(){
+		var fontSize = $(this).attr('data-fontSize');
+		$('#paragraph').css('font-size', fontSize);
+	})
+})
+$(document).ready(function(){
+	$('.changeFont').on("click", function(){
+		var font = $(this).attr('data-font');
+		$('#paragraph').css('font-family', font);
+	})
+})
